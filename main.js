17b6ad89cd8b5a7bf49d61a00e7bda2525116b02
GhuzzBeatz <gustavosouzashacurr@gmail.com>
2026-06-22 18:59:50 -0300

    Secure license gate and standardize GHZ support

--- main.js
+++ main.js
@@ -2,9 +2,30 @@
 const path = require('path')
 const fs = require('fs')
 
+const gotSingleInstanceLock = app.requestSingleInstanceLock()
+if (!gotSingleInstanceLock) app.quit()
+
+let win = null
+let ghzBackend = null
+
+app.on('second-instance', () => {
+  if (!win) return
+  if (win.isMinimized()) win.restore()
+  win.show()
+  win.focus()
+})
+
+function isLicensePageUrl(url) {
+  try { return decodeURIComponent(new URL(url).pathname).replace(/\\/g, '/').endsWith('/pages/licenca.html') } catch (e) { return false }
+}
+
+function loadLicensePage() {
+  if (win && !win.isDestroyed()) win.loadFile('pages/licenca.html').catch(() => {})
+}
+
 function createWindow() {
   const dataDir = getDataDir()
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1460,
     height: 920,
     minWidth: 1180,
@@ -22,12 +43,18 @@ function createWindow() {
       nodeIntegration: true,
       nodeIntegrationInSubFrames: true,
       contextIsolation: false,
+      devTools: !app.isPackaged,
       additionalArguments: [`--data-dir=${dataDir}`]
     },
     icon: path.join(__dirname, 'assets', 'icon.png')
   })
 
-  win.loadFile('index.html')
+  win.webContents.on('will-navigate', (event, url) => {
+    if (!ghzBackend?.isSessionAuthorized() && !isLicensePageUrl(url)) {
+      event.preventDefault()
+      loadLicensePage()
+    }
+  })
 }
 
 function getDataDir() {
@@ -36,13 +63,18 @@ function getDataDir() {
   return dataDir
 }
 
-app.whenReady().then(() => {
-  require('./js/ghz-backend')({
+ghzBackend = require('./js/ghz-backend')({
     app, ipcMain, getDataDir,
     appId: 'imobcore',
     manifestUrl: 'https://raw.githubusercontent.com/GhuzzBeatz/ImobCore/master/update-manifest.json'
-  })
+})
+
+app.whenReady().then(async () => {
+  if (!gotSingleInstanceLock) return
   createWindow()
+  await win.loadFile('pages/licenca.html')
+  const result = await ghzBackend.validateForStartup().catch(() => ({ ok: false }))
+  if (result?.ok && win && !win.isDestroyed()) await win.loadFile('index.html')
 })
 
 app.on('window-all-closed', () => {
